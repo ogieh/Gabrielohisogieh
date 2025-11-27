9406c08131debcdf253ef2914b7539d91eb86929
Gabriel <125802297+ogieh@users.noreply.github.com>
2025-11-27 09:58:13 +0100

    Create physics_formulars.py

---
+++ physics_formulars.py
@@ -0,0 +1,15 @@
+def newtons_second_law():
+    Mass = float(input("Enter the mass of the object (in Kilogram): "))
+    Acceleration = float(input("Enter the accelaration of the object (in meters per second square): "))
+    Force = Mass * Acceleration
+    print("The force at which the object is moving is: ", Force)
+
+
+
+
+def main():
+    print("1. Newton's second law")
+    print("2. ohm's law")
+    print("3. kinectic energy")
+    print("4. Velocity")
+    print("5. Impulsive momentum")
